c56fd3ecf02b57ba841d8905d751e99a06a71348
Jose-Ilafaya <ilafayajose@gmail.com>
2022-09-16 01:08:14 -0300

    h

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -4,6 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminPageComponent } from './admin-page/admin-page.component';
 
+//importamos librerias de FireStore y el servicio Cafe
+import {AngularFireModule} from '@angular/fire/compat'
+import { environment } from 'src/environments/environment';
+import { ReactiveFormsModule } from '@angular/forms';
+import {CafesService} from '../../servicios/cafes.service'
 
 @NgModule({
   declarations: [
@@ -11,7 +16,10 @@ import { AdminPageComponent } from './admin-page/admin-page.component';
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
-  ]
+    AdminRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    ReactiveFormsModule
+  ],
+  providers: [CafesService]
 })
 export class AdminModule { }
